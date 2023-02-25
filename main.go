989e2e28d5b07ae3578f85090fb90d0c4815ef19
PaiGack <460852283@qq.com>
2023-02-25 18:25:47 +0800

    feat go

--- main.go
+++ main.go
@@ -4,17 +4,30 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"sync"
+	"time"
 )
 
 func main() {
-	for i := 21; i < 100; i++ {
-		address := fmt.Sprintf("127.0.0.1:%d", i)
-		conn, err := net.Dial("tcp", address)
-		if err != nil {
-			log.Printf("%s cloesd\n", address)
-			continue
-		}
-		conn.Close()
-		log.Printf("%s opened\n", address)
+	start := time.Now()
+	wg := sync.WaitGroup{}
+
+	for i := 1; i < 65536; i++ {
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+
+			address := fmt.Sprintf("127.0.0.1:%d", i)
+			conn, err := net.Dial("tcp", address)
+			if err != nil {
+				log.Printf("%s cloesd\n", address)
+				return
+			}
+			conn.Close()
+			log.Printf("%s opened\n", address)
+		}(i)
 	}
+
+	wg.Wait()
+	log.Printf("cost %f Seconds", time.Since(start).Abs().Seconds())
 }
