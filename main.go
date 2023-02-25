e34fb4e3f8e9d651cb7cb0a06225491b48b84f11
PaiGack <460852283@qq.com>
2023-02-25 18:58:01 +0800

    feat go worker pool

--- main.go
+++ main.go
@@ -4,30 +4,59 @@ import (
 	"fmt"
 	"log"
 	"net"
-	"sync"
+	"sort"
 	"time"
 )
 
+func worker(ports chan int, port chan int) {
+	for p := range ports {
+		address := fmt.Sprintf("127.0.0.1:%d", p)
+		conn, err := net.Dial("tcp", address)
+		if err != nil {
+			port <- -p
+			continue
+		}
+		conn.Close()
+		port <- p
+	}
+}
+
 func main() {
 	start := time.Now()
-	wg := sync.WaitGroup{}
 
-	for i := 1; i < 65536; i++ {
-		wg.Add(1)
-		go func(i int) {
-			defer wg.Done()
+	ports := make(chan int, 1024*2*2)
+	result := make(chan int)
+	const COUNT = 65535
 
-			address := fmt.Sprintf("127.0.0.1:%d", i)
-			conn, err := net.Dial("tcp", address)
-			if err != nil {
-				log.Printf("%s cloesd\n", address)
-				return
-			}
-			conn.Close()
-			log.Printf("%s opened\n", address)
-		}(i)
+	go func() {
+		for i := 1; i <= COUNT; i++ {
+			ports <- i
+		}
+	}()
+
+	for i := 0; i < cap(ports); i++ {
+		go worker(ports, result)
 	}
 
-	wg.Wait()
+	var open_ports []int
+	var closed_ports []int
+	for i := 0; i < COUNT; i++ {
+		port := <-result
+		if port <= 0 {
+			closed_ports = append(closed_ports, -port)
+		} else {
+			open_ports = append(open_ports, port)
+		}
+	}
+
+	sort.Ints(open_ports)
+	sort.Ints(closed_ports)
+
+	log.Printf("closed ports %v", closed_ports)
+	log.Printf("open ports %v", open_ports)
+
+	close(ports)
+	close(result)
+
 	log.Printf("cost %f Seconds", time.Since(start).Abs().Seconds())
 }
